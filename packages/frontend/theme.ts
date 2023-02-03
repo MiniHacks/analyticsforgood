5dd5175c04d9131374c337b0cde013caa6e04f69
Samyok Nepal <sam@yok.dev>
2023-02-03 12:32:04 -0600

    🥹 add brand colors

--- packages/frontend/theme.ts
+++ packages/frontend/theme.ts
@@ -1,3 +1,17 @@
 import { extendTheme } from "@chakra-ui/react";
 
-export default extendTheme({});
+export default extendTheme({
+  colors: {
+    brand: {
+      100: "#EFFAFA",
+      200: "#CEECEF",
+      300: "#95DAE0",
+      400: "#76CAD4",
+      500: "#58BAC8",
+      600: "#4292A0",
+      700: "#1A434F",
+      800: "#1A434F",
+      900: "#1A434F",
+    },
+  },
+});
