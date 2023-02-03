b1f3c05cbd2d7013d418a548453c1d0e1c8a3fa0
Kanishk-K <kanishkkacholia@gmail.com>
2023-02-03 13:38:00 -0600

    🧽 Cleaning complete, verifying and drying.

--- packages/backend/cleaning.py
+++ packages/backend/cleaning.py
@@ -4,9 +4,7 @@ def money_to_int(money):
     return int(float(money[1:]) * 100)
 
 if __name__ == "__main__":
-    df = pd.read_csv("clean/FY21 Planning Items.csv", dtype={"Product ID":int})
+    df = pd.read_csv("raw/Land_Water Access.csv")
     print(df.dtypes)
     NaNVals = df[df.isnull().any(axis=1)]
     print(NaNVals)
-    df["Cost"] = df["Cost"].apply(money_to_int)
-    df.to_csv("clean/FY21 Planning Items.csv",index=False)
