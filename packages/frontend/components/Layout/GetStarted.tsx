6c3ff8be4ffdba74ac182d568745fd0d6fcc98fc
Brandon <brandonnguyenlam@gmail.com>
2023-02-03 15:37:38 -0600

    😤 animatorerer 😤

--- packages/frontend/components/Layout/GetStarted.tsx
+++ packages/frontend/components/Layout/GetStarted.tsx
@@ -1,18 +1,31 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Box } from "@chakra-ui/react";
+import {useState} from "react";
 
-const GetStarted = (): JSX.Element => (
+const GetStarted = () => {
+    const [hover, setHover] = useState(false);
 
+    const handleHover = () => {
+        setHover(true);
+    };
+
+    const handleLeave = () => {
+        setHover(false);
+    };
+    return (
         <Button
+            onMouseEnter={handleHover}
+            onMouseLeave={handleLeave}
             colorScheme={"brand"}
             /*IMPORT GOTO SOMEWHERE*/
+
             style={{
                 position: "absolute",
                 width: "254.7px",
                 height: "81px",
                 left: "120px",
                 top: "544px",
-                background: "#58BAC8",
-                boxShadow: "6px 6px 0px #CEECEF",
+                background: "brand",
+                boxShadow: hover ? "2px 2px 0px #CEECEF" : "6px 8px 0px #CEECEF",
                 borderRadius: "15px",
                 font: "Poppins",
                 fontStyle: "normal",
@@ -26,6 +39,7 @@ const GetStarted = (): JSX.Element => (
             }}
         >
             Get Started</Button>
-);
+    )
+};
 
 export default GetStarted;
