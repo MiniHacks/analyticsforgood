ea217c0120b0f2102e06e353355ed3728505e508
Brandon <brandonnguyenlam@gmail.com>
2023-02-03 14:02:57 -0600

    😤 examples 😤

---
+++ packages/frontend/components/Layout/ExampleBox.tsx
@@ -0,0 +1,24 @@
+import { Box, Image } from "@chakra-ui/react";
+
+const ExampleBox = (): JSX.Element => (
+    <Box
+        style={{
+        position: "absolute",
+        width: "519px",
+        height: "325px",
+        left: "902.28px",
+        top: "169px",
+        borderColor: "brand.500",
+        border: "5px solid #58BAC8",
+        boxShadow: "6px 6px 0px #CEECEF",
+        borderRadius: "15px",
+        transform: "rotate(8.72deg)",
+        }}
+    >
+            {/*<Image>*/}
+            {/*    <source srcSet="https://i.imgur.com/4ZQZ1Zu.png" />*/}
+            {/*</Image>*/}
+    </Box>
+)
+
+export default ExampleBox
