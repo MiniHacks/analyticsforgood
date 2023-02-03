985c7713f1845ce410cfdbe9303115e10f96673a
Brandon <brandonnguyenlam@gmail.com>
2023-02-03 17:24:16 -0600

    😤 ansh rewrote my code 😤

--- packages/frontend/components/Layout/GetStarted.tsx
+++ packages/frontend/components/Layout/GetStarted.tsx
@@ -1,15 +1,16 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Link } from "@chakra-ui/react";
 
-const GetStarted = (): JSX.Element => (
-
+const GetStarted = () => {
+    return (
         <Button
             colorScheme={"brand"}
-            /*IMPORT GOTO SOMEWHERE*/
             style={{
-                marginTop:"50px",
+                position: "absolute",
                 width: "254.7px",
                 height: "81px",
-                background: "#58BAC8",
+                left: "120px",
+                top: "544px",
+                background: "brand",
                 boxShadow: "6px 6px 0px #CEECEF",
                 borderRadius: "15px",
                 font: "Poppins",
@@ -18,11 +19,22 @@ const GetStarted = (): JSX.Element => (
                 fontSize: "24px",
                 lineHeight: "118%",
                 display: "flex",
+                alignItems: "center",
                 letterSpacing: "0.04em",
                 color: "#FFFFFF",
             }}
+            _hover={{
+                boxShadow: "3px 3px 0px #CEECEF !important",
+                backgroundColor: "brand.600",
+                transform: "translate(3px, 4px)",
+            }}
+
         >
-            Get Started</Button>
-);
+            <Link href={"/dashboard"}>
+                Get Started
+            </Link>
+        </Button>
+    )
+};
 
 export default GetStarted;
