ade427b8362a730fab7ab54f77f04f3a70c3bde2
iamstutishah <shah0794@umn.edu>
2023-02-03 13:30:09 -0600

    😁 added the fonts Inter, Poppins, and Baloo 2

--- packages/frontend/pages/_document.tsx
+++ packages/frontend/pages/_document.tsx
@@ -5,7 +5,14 @@ import theme from "../theme";
 export default function Document(): JSX.Element {
   return (
     <Html lang={"en"}>
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@800&family=Inter:wght@300;400;500&family=Poppins:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
