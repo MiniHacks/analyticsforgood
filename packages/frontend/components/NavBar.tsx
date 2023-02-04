7fdb8e0d37c7e32a5edfaf2f0463312a3443e5d8
iamstutishah <shah0794@umn.edu>
2023-02-03 19:59:24 -0600

    🗣️ added the link for auth but idk

--- packages/frontend/components/NavBar.tsx
+++ packages/frontend/components/NavBar.tsx
@@ -1,21 +1,13 @@
-import {
-  Box,
-  Button,
-  Heading,
-  HStack,
-  VStack,
-  Text,
-  Input,
-} from "@chakra-ui/react";
+import { Box, Button, HStack } from "@chakra-ui/react";
 import Logo from "./Logo";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 
 const NavBar = (): JSX.Element => {
   const { data: session, status } = useSession();
-
   if (status === "authenticated") {
     window.location.href = "/dash";
   }
+
   console.log(status);
   return (
     <Box color={"black"} py={7} bg={"transparent"} top={"0"} zIndex={"100"}>
@@ -31,7 +23,7 @@ const NavBar = (): JSX.Element => {
                 fontSize={"18px"}
                 colorScheme={"white"}
                 color={"brand.600"}
-                onClick={() => signIn()}
+                onClick={() => window.open("https://accounts.google.com/o/oauth2/auth", "_blank")}
               >
                 Login
               </Button>
@@ -46,7 +38,7 @@ const NavBar = (): JSX.Element => {
               color={"brand.600"}
               onClick={() => signOut()}
             >
-              Logout, {session.user.name}
+              Logout {session.user.name}
             </Button>
           )}
           <Button
