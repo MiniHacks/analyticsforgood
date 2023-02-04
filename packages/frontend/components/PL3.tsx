88c330b9c7e024410e05ecb724b1ca75e2e6e3cf
iamstutishah <shah0794@umn.edu>
2023-02-04 06:53:58 -0600

    👾 added spacing and the farmland image

--- packages/frontend/components/PL3.tsx
+++ packages/frontend/components/PL3.tsx
@@ -2,34 +2,35 @@ import { HStack, Box, Text, Heading, VStack, Image } from "@chakra-ui/react";
 import React from "react";
 
 const PL3 = (): JSX.Element => {
-    return (
-        <Box>
-            <HStack
-                marginTop={"200px"}
-                w={"100%"}
-                h={"600px"}
-                bg={"white"}
-                justifyContent={"center"}
-                alignItems={"center"}
-            >
-                <HStack justifyContent={"space-between"} mr={"100px"} ml={"20px"}>
-                    <Image src={'GraphicAly.png'} px={"10%"} maxWidth={"850px"}/>
-                    <VStack>
-                        <Heading fontSize={"50px"}>Problem</Heading>
-                        <Text fontFamily={"Poppins"} fontWeight={"300"} px={8}>
-                            The Good Acre is committed to creating positive change
-                            and supporting our local farming community. Unfortunately,
-                            the issue of "Money on the Table" has arisen, presenting a
-                            setback in the mission. This phrase represents missed opportunities,
-                            where potential funds are left untapped, thereby hindering our ability
-                            to aid underrepresented farmers. Aly is an innovative solution aimed
-                            at resolving the issue
-                        </Text>
-                    </VStack>
-                </HStack>
-            </HStack>
-        </Box>
-    );
+  return (
+    <Box>
+      <HStack
+        marginTop={"200px"}
+        w={"100%"}
+        h={"600px"}
+        bg={"white"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <HStack justifyContent={"space-between"} mr={"100px"} ml={"20px"}>
+          <Image src={"GraphicAly.png"} px={"10%"} maxWidth={"850px"} />
+          <VStack>
+            <Heading fontSize={"50px"}>Problem</Heading>
+            <Text fontFamily={"Poppins"} fontWeight={"300"} px={8} py={3}>
+              The Good Acre is committed to creating positive change and
+              supporting our local farming community.
+              <Box py={3} /> Unfortunately, the issue of "Money on the Table"
+              has arisen, presenting a setback in the mission. <Box py={3} />{" "}
+              This phrase represents missed opportunities, where potential funds
+              are left untapped, thereby hindering our ability to aid
+              underrepresented farmers. Aly is an innovative solution aimed at
+              resolving the issue
+            </Text>
+          </VStack>
+        </HStack>
+      </HStack>
+    </Box>
+  );
 };
 
 export default PL3;
